84ccb562e362dceffc8c06ca78bccea58a255051
nickhpt <nickhpt@users.noreply.github.com>
2019-01-03 15:18:08 +0100

    Update runSubgrad.m

--- runSubgrad.m
+++ runSubgrad.m
@@ -1,50 +1,62 @@
-function [pbjective] = runsubgrd(MAXIT,step)
-%clear all, close all;clc
+function [objective] = runSubgrad(MAXIT,step)
+
 
 load('cs.mat');
 x_sol = x;
 
 %Decomposition of the problem in real and imaginary parts
-X_us2 = [real(X_us); imag(X_us)];
-F_us2 = [real(F_us) -imag(F_us); imag(F_us) real(F_us)];
+X_us2 = [real(X_us); imag(X_us); zeros(128,1)];
+F_us2 = [real(F_us) -imag(F_us); imag(F_us) real(F_us); zeros(128,128) eye(128)];
+
+%Particular solution
+xp = pinv(F_us2)*X_us2;
+
+F = null(F_us2);
 
 %--------------------------------------
 % min norm(x,1) 
 % s.t. F_us*x - X_us = 0
+%      imag(x) = 0
 %      real(x) >= 0
 %
 %--------------------------------------
+rmask = [eye(128) zeros(128,128); zeros(128,128) zeros(128,128)];
+imask = [zeros(128,128) zeros(128,128); zeros(128,128) eye(128)];
+get_x_real = @(z) rmask*(F*z + xp);
+get_x_im = @(z) imask*(F*z + xp);
 
-func = @(x) norm(x(1:128) + 1i*x(129:256), 1);
+
+func = @(z) norm(get_x_real(z)+ 1i*get_x_im(z), 1);
 
 %PROJ = F_us'*inv(F_us*F_us');
-PROJ = pinv(F_us2);
-proj = @(z) z - PROJ*(F_us2*z - X_us2);
+%PROJ = pinv(F_us2);
 
 %sub gradient of norm(x,1) = sign(x).
-grad = @(z) [sign(z(1:128)); zeros(128,1)];
+%grad = @(z) [sign(z(1:128)); zeros(128,1)];
+grad = @(z) F'*sign(F*z + xp);
 
 % Constraint (Ax - b <= 0)
-const = @(z) -[z(1:128); zeros(128,1)];
-grad_const = @(z) -[ones(128,1); zeros(128,1)];
+const = @(z) -get_x_real(z);
+grad_const = @(z) -(rmask*F)';
 
 f = [];
-
-fopt=func([x_sol; zeros(128,1)]);
-
-it = 0;
-
-it2 = 1;
-
-%Starting point: Least square solution
-xk = pinv(F_us2)*X_us2;
-%xk = zeros(256,1);
-
-
-alpha = 0.5;
-beta = 0.9;
-
     
+%MAXIT=500;
+%MAXIT=100;
+it = 0;
+xk = zeros(72,1);
+%xk = pinv(F_us2)*X_us2;
+%incr=1;
+test=0;
+
+
+J = [];
+K = [];
+
+F = [];
+
+BEST_XK = xk;
+
 while(it < MAXIT)
     it = it+1;
     
@@ -57,80 +69,60 @@ while(it < MAXIT)
     
     %Finding most violated contraint, should be positive if violated
     [cmax, ind] = max(const(xk));
-
     
-    if(cmax <= 0)
-        %In this case, the constraint is ok. We use the normal proj subgrad
-        %method
-        g = grad(xk);
-        g = g - ([zeros(128,1); 10*ones(128,1)] == g)/2; %Putting zeros to 0.5
-        
-        stepsize = step/it;
-        
-        %stepsize = (func(xk) - func([x_sol; zeros(128,1)]))/(norm(g,2)^2);
-        
-%         t = 1;
-%         j=0;
-%         
-%         while(j<50 && func(xk - t*g) < func(xk) - alpha*t*g'*g)
-%             
-%             t = beta*t;
-%             j = j+1;
-%             
-%         end
-%         
-%         stepsize = t
-        
-%         tp = -1:0.01:1;
-%         for j=1:length(tp)
-%            y(j) = func(xk - tp(j)*g);
-%            z(j) = func(xk) - alpha*tp(j)*g'*g;
-%         end
-%         
-%         figure(3)
-%         plot(tp,z);
-%         hold on
-%         plot(tp,y);
-        
-        %Update
-        xk = xk - stepsize*(eye(256) - PROJ*F_us2)*g;  
-        
-      
-        
-         
-    else
-        %In this case, the constraint is not ok, xk is not feasible. we set
-        %the step direction as the derivative of the constraint
-        g = zeros(256,1);
+    j = 0;
+    
+    while(j < 1000 && cmax >= 1e-3)
         gc = grad_const(xk);
-        g(ind) = gc(ind);
+        g = gc(:,ind);
         
-        stepsize = abs(cmax)*2;
+        %stepsize = abs(cmax)*2;
+        stepsize = abs(cmax)/norm(g)^2;
         
         xk = xk-stepsize*g;
         
+        [cmax, ind] = max(const(xk));
+        j = j+1;
     end
     
+
+    %In this case, the constraint is ok. We use the normal proj subgrad
+    %method
+    g = grad(xk);
+    %g = g - ([zeros(128,1); 10*ones(128,1)] == g)/10; %Putting zeros to -0.1 to enforce positivity
+
+    %stepsize = 1/it;
     
-    objective(it)=func(xk);
+    switch step
+            case 1
+                stepsize = 1/it;          
+            case 2
+                stepsize = 2/(it);                
+            case 3
+                stepsize = 4/(it); 
+                 
+        end
+        
+
+    %Update
+    xk = xk - stepsize*g; 
     
+    F = [F func(xk)];
+    
+    if func(xk) < func(BEST_XK)
+        BEST_XK = xk;
+    end
+    
+    objective(it)=func(BEST_XK);
     
 end
 
-figure(1)
+x = get_x_real(BEST_XK);
 
+
+fopt=0;
  semilogy((objective(2:end)-fopt),'LineWidth',1.5)
  xlabel('Number of Iterations');ylabel('f(x_k)-f^*');
  grid on
- hold on
-
-
-
-figure(2)
-
-plot(xk(1:128))
-hold on;
-
-
 end
 
