923c15d1c04b8f06b17ecc3179a8d7acc421d293
bjoukovs <borisjoukov@hotmail.com>
2019-01-03 14:37:31 +0100

    working on reduced problem

--- projectedSubgradient_v2reduced.m
+++ projectedSubgradient_v2reduced.m
@@ -7,6 +7,11 @@ x_sol = x;
 X_us2 = [real(X_us); imag(X_us); zeros(128,1)];
 F_us2 = [real(F_us) -imag(F_us); imag(F_us) real(F_us); zeros(128,128) eye(128)];
 
+%Particular solution
+xp = pinv(F_us2)*X_us2;
+
+F = null(F_us2);
+
 %--------------------------------------
 % min norm(x,1) 
 % s.t. F_us*x - X_us = 0
@@ -14,27 +19,32 @@ F_us2 = [real(F_us) -imag(F_us); imag(F_us) real(F_us); zeros(128,128) eye(128)]
 %      real(x) >= 0
 %
 %--------------------------------------
+rmask = [eye(128) zeros(128,128); zeros(128,128) zeros(128,128)];
+imask = [zeros(128,128) zeros(128,128); zeros(128,128) eye(128)];
+get_x_real = @(z) rmask*(F*z + xp);
+get_x_im = @(z) imask*(F*z + xp);
 
-func = @(x) norm(x(1:128) + 1i*x(129:256), 1);
+
+func = @(z) norm(get_x_real(z)+ 1i*get_x_im(z), 1);
 
 %PROJ = F_us'*inv(F_us*F_us');
-PROJ = pinv(F_us2);
+%PROJ = pinv(F_us2);
 
 %sub gradient of norm(x,1) = sign(x).
 %grad = @(z) [sign(z(1:128)); zeros(128,1)];
-grad = @(z) sign(z);
+grad = @(z) F'*sign(F*z + xp);
 
 % Constraint (Ax - b <= 0)
-const = @(z) -[z(1:128); zeros(128,1)];
-grad_const = @(z) -[ones(128,1); zeros(128,1)];
+const = @(z) -get_x_real(z);
+grad_const = @(z) -(rmask*F)';
 
 f = [];
     
-MAXIT=1e5;
+MAXIT=1e4;
 %MAXIT=100;
 it = 0;
-%xk = zeros(256,1);
-xk = pinv(F_us2)*X_us2;
+xk = zeros(72,1);
+%xk = pinv(F_us2)*X_us2;
 %incr=1;
 test=0;
 
@@ -43,6 +53,7 @@ alpha = 0.5;
 beta = 0.1;
 
 J = [];
+K = [];
 
 while(it < MAXIT)
     it = it+1;
@@ -62,50 +73,53 @@ while(it < MAXIT)
         %In this case, the constraint is ok. We use the normal proj subgrad
         %method
         g = grad(xk);
-        g = g - ([zeros(128,1); 10*ones(128,1)] == g)/10; %Putting zeros to -0.1 to enforce positivity
+        %g = g - ([zeros(128,1); 10*ones(128,1)] == g)/10; %Putting zeros to -0.1 to enforce positivity
         
-        stepsize = 0.95/it;
+        stepsize = 1/it;
         
-%          t = 1;
-%         j=0;
-%         
-%         while(j<10 && func(xk - alpha*t*g) > func(xk) - alpha*t*g'*g)
-%             
-%             t = beta*t;
-%             j = j+1;
-%             
-%         end
-% 
-%         
-%         
-%         stepsize = alpha*t;
-%         J = [J stepsize];
+         t = 1;
+        j=0;
+        
+        while(j<10 && func(xk - alpha*t*g) < func(xk) - alpha*t*g'*g)
+            
+            t = beta*t;
+            j = j+1;
+            
+        end
+
+        
+        
+        stepsize = alpha*t;
+        J = [J stepsize];
+        K = [K 1];
 
         %Update
-        xk = xk - stepsize*(eye(256) - PROJ*F_us2)*g; 
-        
+        xk = xk - stepsize*g; 
+
          
     else
         %In this case, the constraint is not ok, xk is not feasible. we set
         %the step direction as the derivative of the constraint
-        g = zeros(256,1);
         gc = grad_const(xk);
-        g(ind) = gc(ind);
+        g = gc(:,ind);
         
         %stepsize = abs(cmax)*2;
         stepsize = abs(cmax)/norm(g)^2;
         
         xk = xk-stepsize*g;
         
+        
+        K = [K -1];
+        
     end
     
     
 end
 
+x = get_x_real(xk);
+
 subplot(2,1,1)
-plot(xk(1:128))
-hold on
-plot(xk(129:256))
+plot(x(1:128))
 subplot(2,1,2)
 plot(x_sol)
 
