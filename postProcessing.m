54d64614cd76ecb3461ec3db4cf7edbb5299e123
nickhpt <nickhpt@users.noreply.github.com>
2019-01-04 13:48:53 +0100

    Update postProcessing.m

--- postProcessing.m
+++ postProcessing.m
@@ -1,16 +1,31 @@
 close all;clear all; clc
 
-maxit=1e2;
-x = runSubgrad(maxit,1);
+%Reduced subgradient plots
+maxit=200;
+%Second argument is stepsize n/it
+stepsizes = [0.1 0.5 1 2 4 6];
+legends = {};
+
+for i=1:length(stepsizes)
+
+runSubgradReduced(maxit,stepsizes(i)); 
 hold on
-x2 = runSubgrad(maxit,2);
-hold on
-x3 = runSubgrad(maxit,3);
-legend('1/k','2/k','4/k');
+
+legends{i} = strcat(num2str(stepsizes(i)),'/k');
+
+end
+
+legend(legends);
 title(['Convergence with various stepsizes',' ','MAXIT' ' =',' ',num2str(maxit)])
 
-figure
-plot(x)
+%%
+%Projected subgradient plots
+maxit=1e3;
+%Second argument is stepsize n/it
+runSubgradProj(maxit,1); 
 hold on
-plot(x2)
-plot(x3)
+runSubgradProj(maxit,2);
+hold on
+runSubgradProj(maxit,3);
+legend('1/k','2/k','4/k');
+title(['Convergence with various stepsizes',' ','MAXIT' ' =',' ',num2str(maxit)])
