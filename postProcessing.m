e20970f399ee66f4303b05a0ee74311e940ba4bb
nickhpt <nickhpt@users.noreply.github.com>
2019-01-04 18:32:32 +0100

    Update postProcessing.m

--- postProcessing.m
+++ postProcessing.m
@@ -1,31 +1,61 @@
 close all;clear all; clc
-
+load('cs.mat');
 %Reduced subgradient plots
-maxit=200;
+maxit=1000;
 %Second argument is stepsize n/it
-stepsizes = [0.1 0.5 1 2 4 6];
+%stepsizes = [0.1 0.2 0.5 1];
+stepsizes = (0.1:0.1:1);
+
 legends = {};
 
 for i=1:length(stepsizes)
 
-runSubgradReduced(maxit,stepsizes(i)); 
+[numit(i),xsolved]=runSubgradReduced(maxit,stepsizes(i)); 
+hold on
+
+legends{i} = strcat(num2str(stepsizes(i)),'/k');
+
+legend(legends);
+title(['Convergence with various stepsizes',' ','MAXIT' ' =',' ',num2str(maxit)])
+
+
+end
+
+% figure
+% subplot(2,1,1)
+% plot(xsolved(1:128));
+% legend('Reduced subgradiant')
+% subplot(2,1,2);
+% plot(x);
+% legend('True solution');
+
+%sgtitle('Comparison of solutions')
+
+
+%%
+plot(objective);
+
+%%
+plot(stepsizes,numit,'b','LineWidth',2);
+ylabel('# Iterations');xlabel('Stepsize \alpha')
+grid on
+%%
+maxit=100;
+[numit,objective]=runSubgradReduced(maxit,0.1); 
+
+%%
+%Projected subgradient plots
+maxit=1000;
+%Second argument is stepsize n/it
+stepsizes = [0.1 0.2];
+%stepsizes = (0.1:0.1:1);
+
+legends = {};
+
+for i=1:length(stepsizes)
+[numit(i)]=runSubgradReduced(maxit,stepsizes(i)); 
 hold on
 
 legends{i} = strcat(num2str(stepsizes(i)),'/k');
 
 end
-
-legend(legends);
-title(['Convergence with various stepsizes',' ','MAXIT' ' =',' ',num2str(maxit)])
-
-%%
-%Projected subgradient plots
-maxit=1e3;
-%Second argument is stepsize n/it
-runSubgradProj(maxit,1); 
-hold on
-runSubgradProj(maxit,2);
-hold on
-runSubgradProj(maxit,3);
-legend('1/k','2/k','4/k');
-title(['Convergence with various stepsizes',' ','MAXIT' ' =',' ',num2str(maxit)])
