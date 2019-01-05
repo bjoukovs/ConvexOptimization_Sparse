427102c5dfc24a0fd23620f4876d96d0027e04fe
nickhpt <nickhpt@users.noreply.github.com>
2019-01-05 01:19:51 +0100

    postprocessing final

--- postProcessing.m
+++ postProcessing.m
@@ -1,8 +1,9 @@
 close all;clear all; clc
 load('cs.mat');
+%*************************
 %Reduced subgradient plots
+%*************************
 maxit=1000;
-%Second argument is stepsize n/it
 %stepsizes = [0.1 0.2 0.5 1];
 stepsizes = (0.1:0.1:1);
 
@@ -31,31 +32,37 @@ end
 
 %sgtitle('Comparison of solutions')
 
-
-%%
-plot(objective);
-
 %%
 plot(stepsizes,numit,'b','LineWidth',2);
 ylabel('# Iterations');xlabel('Stepsize \alpha')
 grid on
-%%
-maxit=100;
-[numit,objective]=runSubgradReduced(maxit,0.1); 
+title('Iterations with stepped \alpha');
 
 %%
+%***************************
 %Projected subgradient plots
-maxit=1000;
-%Second argument is stepsize n/it
-stepsizes = [0.1 0.2];
+%**************************
+maxit=1e5;
+stepsizes = [0.1 0.2 0.5 1];
 %stepsizes = (0.1:0.1:1);
 
 legends = {};
-
 for i=1:length(stepsizes)
-[numit(i)]=runSubgradReduced(maxit,stepsizes(i)); 
+    
+tic
+[xsolved]=runSubgradProj(maxit,stepsizes(i)); 
+toc
 hold on
-
 legends{i} = strcat(num2str(stepsizes(i)),'/k');
+legend(legends);
+title(['Convergence with various stepsizes',' ','MAXIT' ' =',' ',num2str(maxit)])
 
 end
+
+figure
+subplot(2,1,1)
+plot(xsolved(1:128));
+legend('Projected subgradiant')
+subplot(2,1,2);
+plot(x);
+legend('True solution');
